042d751b31364d46ff8934cda48cb42cdfca15b9
Nitin <chdnitinsharma@gmail.com>
2021-09-11 12:42:00 +0530

    first stateless function

---
+++ src/app-stateless-func.js
@@ -0,0 +1,16 @@
+var appRoot = document.getElementById("app");
+
+const User = ()=>{
+
+    return (
+        <div>
+        <h1>TEsting my stateless function</h1>
+
+        <p>Welcome to stateless function</p>
+        </div>
+    );
+
+}
+
+
+ReactDOM.render(<User />, appRoot );
